85cfc0aabc83d50212759c5ada8bb35e8604cc1c
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-01-20 13:08:41 +0000

    Now with pictures

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 def index(request):
      context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
-    html="Welcome page" +'<a href="/rango/about/">About</a>'
-    return render(request,'rango/index.html',context=context_dict)
+     html="Welcome page" +'<a href="/rango/about/">About</a>'
+     return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
     html="About page" + '<a href="/rango/">Index</a>'
