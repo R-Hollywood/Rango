f5a1845e5e08e31958e15cd66f121b1c1d1c4a18
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-01-20 12:47:08 +0000

    With Template

--- rango/views.py
+++ rango/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
- html="Welcome page" +'<a href="/rango/about/">About</a>'
- return HttpResponse(html)
-
+     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    html="Welcome page" +'<a href="/rango/about/">About</a>'
+    return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
     html="About page" + '<a href="/rango/">Index</a>'
