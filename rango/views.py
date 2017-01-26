9d5bb6d9c1f90e49c2cdf3003285f4084cd4cdcc
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-01-26 19:47:10 +0000

    chapter 6 working

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from rango.models import Category, Page
 def index(request):
-     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+     category_list = Category.objects.order_by('-likes')[:5]
+     page_list = Page.objects.order_by('-views')[:5]
+     context_dict = {'pages':page_list,'categories':category_list,'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
      
      return render(request,'rango/index.html',context=context_dict)
 
@@ -10,3 +12,15 @@ def about(request):
      context_dict = {'boldmessage': "This tutorial has been put together by" ,'name':"Rachel-Anne Hollywood"}
      
      return render(request,'rango/about.html',context=context_dict)
+
+def show_category(request,category_name_slug):
+     context_dict={}
+     try:
+          category=Category.objects.get(slug=category_name_slug)
+          pages=Page.objects.filter(category=category)
+          context_dict['pages'] = pages
+          context_dict['category'] = category
+     except Category.DoesNotExist:
+          context_dict['category'] = None
+          context_dict['pages'] = None
+     return render(request,'rango/category.html',context_dict)
