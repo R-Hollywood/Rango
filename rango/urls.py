9cc87f95b2278409da669115af3edfaf0591273e
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-02-02 14:20:32 +0000

    Chapter 8 halfway point

--- rango/urls.py
+++ rango/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from rango import views
 
+app_name = 'rango'
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^about/',views.about, name='about'),
