17df3b300ec10009b4aa6aac69a3434bc7862889
Rachel-Anne Hollywood <2195303H@student.gla.ac.uk>
2017-02-06 22:24:08 +0000

    Chapter 9 complete

--- rango/views.py
+++ rango/views.py
@@ -30,6 +30,7 @@ def show_category(request,category_name_slug):
           context_dict['pages'] = None
      return render(request,'rango/category.html',context_dict)
 
+@login_required
 def add_category(request):
      form = CategoryForm()
      if request.method == 'POST':
@@ -43,6 +44,8 @@ def add_category(request):
      return render(request,'rango/add_category.html',{'form':form})
 
 from rango.forms import PageForm
+
+@login_required
 def add_page(request, category_name_slug):
      try:
           category = Category.objects.get(slug=category_name_slug)
@@ -116,4 +119,4 @@ def user_logout(request):
      return HttpResponseRedirect(reverse('index'))
 @login_required
 def restricted(request):
-     return HttpResponse("Since you're logged in, you can see this text!")
+     return render(request,'rango/restricted.html',{})
